4eca69395fdb8a1cb0fda5db2cc889f9de9a0c9a
karpenkoea <ps4karpenko@gmail.com>
2022-09-29 17:40:16 +0300

    some common styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def currently_at(current_page = '')
+    render partial: 'shared/header', locals: { current_page: current_page }
+  end
 
   def full_title(page_title = '')
     base_title = 'Weather Widget'
