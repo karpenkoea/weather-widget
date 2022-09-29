59831328389e7b8abcf3481bde55055f486d43d5
karpenkoea <ps4karpenko@gmail.com>
2022-09-29 20:26:47 +0300

    footer, helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def nav_tab(title, url, options = {})
+    current_page = options.delete :current_page
+    css_class = options[:class].presence || ''
+    options[:class] = current_page == title ? "#{css_class} active" : css_class
+    link_to title, url, options
+  end
+
   def currently_at(current_page = '')
     render partial: 'shared/header', locals: { current_page: current_page }
   end
